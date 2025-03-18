b4fd356e146d0d24948ade69b2ca73631af92c06
Pavel Yurtaev <p.yurtaev@carx-tech.com>
2025-03-18 12:54:44 +0300

    CompleteTZ_3_4_Task_01

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TZ_myWork
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Spawner spawner;
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                Debug.Log("X key down");
+                spawner.Spawn();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Debug.Log("Z key down");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Space key down");
+            }
+        }
+    }
+}
